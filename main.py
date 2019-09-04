daef70e5707db19d9d6ebb94f494c497a991b481
Victor-Pessolato <victor.pessolato@gmail.com>
2019-09-04 17:17:24 +0200

    Implemented Flask's session functionality to keep track of the user's sessions

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from modules import auth
+import os
+from flask import Flask, render_template, request, jsonify
 
-from flask import Flask, render_template, request, jsonify, session
-from flask_session import Session
+from modules import auth
 
 app = Flask(__name__)
 
-# Set the configuration for a server-side session
-app.config.from_object(__name__)
-Session(app)
+app.secret_key = os.environ['SESSION_SECRET_KEY']
+auth.google_signin_key = os.environ['GOOGLE_SIGNIN_KEY']
 
 
 @app.route('/')
@@ -17,7 +16,8 @@ def root():
 
 @app.route('/get-user-data', methods=['GET'])
 def get_user_data():
-    user_info = auth.check_token(request)
+    user_info = auth.authenticate(request)
+    # print(user_info)
     if user_info:
         return render_template('partials/user_info.html', user_info=user_info)
     else:
--- modules/auth.py
+++ modules/auth.py
@@ -1,23 +1,34 @@
-import os
-
+from flask import session
 from google.auth.transport import requests
 from google.oauth2 import id_token
 
-google_signin_key = os.environ['GOOGLE_SIGNIN_KEY']
+google_signin_key = None
 
 
-def check_token(request):
+def identify(token):
+    idinfo = id_token.verify_oauth2_token(
+                token,
+                requests.Request(),
+                google_signin_key)
+
+    if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+        raise ValueError('Wrong issuer.')
+
+    return idinfo
+
+
+def authenticate(request):
     try:
-        token = request.headers.get('X-User-Token')
-        idinfo = id_token.verify_oauth2_token(
-            token,
-            requests.Request(),
-            google_signin_key)
-
-        if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
-            raise ValueError('Wrong issuer.')
-
-        user_info = idinfo
+        user_session = session.get('user_info', None)
+        print(user_session)
+        # If user session is still open, we ignore the token sent.
+        if user_session:
+            user_info = user_session
+        else:
+            # If there is no user session, we check if the token is from a valid user.
+            token = request.headers.get('X-User-Token')
+            user_info = identify(token)
+            session['user_info'] = user_info
 
         return user_info
 
